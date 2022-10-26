3328f3fcd3d744047c5361a08d9d6d810ee2edd5
basit33 <shahbasit741@gmail.com>
2022-10-26 16:14:10 +0500

    made this project with html css and javascript

---
+++ index.js
@@ -0,0 +1,21 @@
+// Variables
+const container = document.querySelector(".container");
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+
+// Event Listners
+left.addEventListener("mouseenter", function () {
+  container.classList.add("hover-left");
+});
+
+left.addEventListener("mouseleave", function () {
+  container.classList.remove("hover-left");
+});
+
+right.addEventListener("mouseenter", function () {
+  container.classList.add("hover-right");
+});
+
+right.addEventListener("mouseleave", function () {
+  container.classList.remove("hover-right");
+});
